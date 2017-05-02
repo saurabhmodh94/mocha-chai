582eca6f0d0ef0815de4c2e31779f59644f74f1f
Saurabh Modh <saurabh.modh@hashedin.com>
2017-05-03 02:26:44 +0530

    addNumber testcases

--- test/appTest.js
+++ test/appTest.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
 
 describe('App', function() {
 	it('app should return hello', function() {
@@ -10,5 +11,15 @@ describe('App', function() {
 	it('sayHello should return string', function() {
 		let result = sayHello();
 		assert.typeOf(result, 'string');
-	})
+	});
+
+	it('addNumbers should return value more than 5', function() {
+		let result = addNumbers(5, 5);
+		assert.isAbove(result, 5);
+	});
+
+	it('addNumbers should return number', function() {
+		let result = addNumbers(5, 5);
+		assert.typeOf(result, 'number');
+	});
 })
