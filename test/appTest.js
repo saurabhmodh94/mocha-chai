2bb7107eb9ea6d7f4a65691fbcaad7e1cfdb014d
Saurabh Modh <saurabh.modh@hashedin.com>
2017-05-03 02:18:17 +0530

    better way to write testcase

--- test/appTest.js
+++ test/appTest.js
@@ -1,8 +1,9 @@
 const assert = require('chai').assert;
-const app = require('../app');
+const sayHello = require('../app').sayHello;
 
 describe('App', function() {
 	it('app should return hello', function() {
-		assert.equal(app(),'hello');
+		let result = sayHello();
+		assert.equal(result, 'hello');
 	})
 })
